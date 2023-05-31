a2b13f1f41bfc71f0daf0183ddb47c54c967bf82
Jung Hyun Woo <1995hyunwoo@naver.com>
2023-05-31 14:04:00 +0000

    chore (coordinateCal) 불필요한 CoordinateCalculator 삭제

--- src/main/java/coordinateCalulator/domain/CoordinateCalulator.java
+++
@@ -1,9 +0,0 @@
-package coordinateCalulator.domain;
-
-public class CoordinateCalulator {
-
-    public static double lineDistance(Point a, Point b) {
-        return Math.sqrt((Math.pow(a.getX() - b.getX(), 2) + (Math.pow(a.getY() - b.getY(), 2))));
-    }
-    
-}
