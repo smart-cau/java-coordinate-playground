135cf567f5034234a41d7b089db64a8bc863ca6d
Jung Hyun Woo <1995hyunwoo@naver.com>
2023-05-31 14:00:37 +0000

    refactor & test (coordinateCal) 두 점 거리 계산 class 구조 변경 및 사각형 계산 테스트 로직 작성

--- src/test/java/coordinateCalulator/CoordinateCalulatorTest.java
+++ src/test/java/coordinateCalulator/CoordinateCalulatorTest.java
@@ -4,17 +4,54 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.offset;
 
-import coordinateCalulator.domain.CoordinateCalulator;
-import coordinateCalulator.domain.Point;
+import coordinateCalulator.domain.Points;
+import coordinateCalulator.domain.calculators.CalcualtorFactory;
+import coordinateCalulator.domain.calculators.Calculator;
+import coordinateCalulator.domain.calculators.LineCalculator;
+import coordinateCalulator.domain.calculators.RectangleCalculator;
 
 public class CoordinateCalulatorTest {
 
+    String input;
+    Points points;
+
+    @Test
+    void lineCalcualtorFactory_test() {
+        input = "(10,10)-(14,15)";
+        points = new Points(input);
+
+        Calculator calculator = CalcualtorFactory.getCalculator(points);
+
+        assertThat(calculator.getClass()).isEqualTo(new LineCalculator(points).getClass());
+    }
+
+    @Test
+    void rectangleCalcualtorFactory_test() {
+        input = "(10,10)-(14,15)-(10,10)-(14,15)";
+        points = new Points(input);
+
+        Calculator calculator = CalcualtorFactory.getCalculator(points);
+
+        assertThat(calculator.getClass()).isEqualTo(new RectangleCalculator(points).getClass());
+    }
+
     @Test
     void lineDistance_test() {
-        Point a = new Point(0, 0);
-        Point b = new Point(5, 5);
+        input = "(0,0)-(5,5)";
+        points = new Points(input);
 
-        assertThat(CoordinateCalulator.lineDistance(a, b))
+        Calculator lineCalculator = CalcualtorFactory.getCalculator(points);
+
+        assertThat(lineCalculator.getResult())
                 .isEqualTo(Math.sqrt(50), offset(0.0001));
+    }
+
+    @Test
+    void rectangleCalcualte_test() {
+        input = "(10,10)-(22,10)-(22,18)-(10,18)";
+        points = new Points(input);
+        Calculator calculator = CalcualtorFactory.getCalculator(points);
+
+        assertThat(calculator.getResult()).isEqualTo(96, offset(0.0001));
     }
 }
