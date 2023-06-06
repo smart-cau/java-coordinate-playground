d62d575d5ffc523334c4ba3dfde001b6690858fe
Jung Hyun Woo <1995hyunwoo@naver.com>
2023-06-06 13:51:05 +0000

    refactor (coordinate) LineTest distance_test -> area_test 변경

--- src/test/java/coordinate/model/figures/LineTest.java
+++ src/test/java/coordinate/model/figures/LineTest.java
@@ -3,23 +3,23 @@ package coordinate.model.figures;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.offset;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import coordinate.model.Point;
 
 public class LineTest {
-    Point point;
-
-    @BeforeEach
-    void setup() {
-        point = new Point(10, 10);
-    }
 
     @Test
-    void distance_test() {
-        Point pointB = new Point(14, 15);
+    void area_test() {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(10, 10));
+        points.add(new Point(14, 15));
 
-        assertThat(point.distance(pointB)).isEqualTo(6.403124, offset(0.01));
+        Line line = new Line(points);
+
+        assertThat(line.area()).isEqualTo(6.403124, offset(0.01));
     }
 }
