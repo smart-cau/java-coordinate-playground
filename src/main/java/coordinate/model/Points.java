29bed451db17dc825834a5ab50b0a69f004f4056
Jung Hyun Woo <1995hyunwoo@naver.com>
2023-06-06 08:04:55 +0000

    refactor (coordinate) Points에 illegalArgumentException case 추가와 equals method 재정의

--- src/main/java/coordinate/model/Points.java
+++ src/main/java/coordinate/model/Points.java
@@ -1,6 +1,8 @@
 package coordinate.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
 public class Points {
 
@@ -9,6 +11,7 @@ public class Points {
     private static final String WRONG_ARGUMENT = "Point의 개수는 " +
             MIN + "이상 " +
             MAX + "이하 여야 합니다.";
+    private static final String ERROR_DUPLICATED_POINTS = "중복된 좌표는 입력할 수 없습니다.";
 
     private final List<Point> points;
 
@@ -18,14 +21,41 @@ public class Points {
     }
 
     private void pointsValidation(List<Point> points) {
+        if (points == null)
+            throw new IllegalArgumentException(WRONG_ARGUMENT);
         if (points.size() < 2)
             throw new IllegalArgumentException(WRONG_ARGUMENT);
         if (points.size() > 4)
             throw new IllegalArgumentException(WRONG_ARGUMENT);
+        if (points.size() != new HashSet<>(points).size())
+            throw new IllegalArgumentException(ERROR_DUPLICATED_POINTS);
     }
 
     public Point get(int index) {
         return points.get(index);
     }
 
+    public boolean anyMatch(int x, int y) {
+        return this.points.stream()
+                .anyMatch((point) -> point.isSame(x, y));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || this.getClass() != o.getClass())
+            return false;
+        Points other = (Points) o;
+        for (int i = 0; i < this.points.size(); i++) {
+            if (!this.points.get(i).equals(other.points.get(i)))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.points);
+    }
 }
--- src/test/java/coordinate/model/PointsTest.java
+++ src/test/java/coordinate/model/PointsTest.java
@@ -1,17 +1,23 @@
 package coordinate.model;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointsTest {
     List<Point> pointList = new ArrayList<>();
     Points points;
+
+    @Test
+    void IllegalArgumentException_null_test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            points = new Points(null);
+        });
+    }
 
     @Test
     void IllegalArgumentException_empty_test() {
@@ -22,6 +28,16 @@ public class PointsTest {
 
     @Test
     void IllegalArgumentException_onePoint_test() {
+        pointList.add(new Point(1, 2));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            points = new Points(pointList);
+        });
+    }
+
+    @Test
+    void IllegalArgumentException_duplicated_points_test() {
+        pointList.add(new Point(1, 2));
         pointList.add(new Point(1, 2));
 
         assertThrows(IllegalArgumentException.class, () -> {
@@ -49,5 +65,26 @@ public class PointsTest {
         points = new Points(pointList);
 
         assertThat(points.get(0)).isEqualTo(new Point(1, 2));
+    }
+
+    @Test
+    void equals_test() {
+        pointList.add(new Point(1, 2));
+        pointList.add(new Point(2, 2));
+        points = new Points(pointList);
+
+        Points other = new Points(pointList);
+
+        assertThat(points).isEqualTo(other);
+    }
+
+    @Test
+    void anyMatch_test() {
+        pointList.add(new Point(1, 2));
+        pointList.add(new Point(2, 2));
+        points = new Points(pointList);
+
+        assertThat(points.anyMatch(1, 2)).isTrue();
+        assertThat(points.anyMatch(4, 2)).isFalse();
     }
 }
